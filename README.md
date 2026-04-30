e101ef0e0b6cb46355064330b5626b221efdc82a
Cabohne <79906057+Cabohne@users.noreply.github.com>
2026-04-30 11:58:07 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# ioBroker.stromgedacht
+
+Release-fertiger Adapter für die StromGedacht API.
+
+## Features
+- Current Status
+- Forecast Daten
+- Konfigurierbares Intervall
+
+## Installation
+iobroker add /pfad/zum/ordner
+
+## Version
+0.2.0
---
+++ src/main.ts
@@ -0,0 +1,67 @@
+
+import utils from "@iobroker/adapter-core";
+import axios from "axios";
+
+class StromGedacht extends utils.Adapter {
+    private interval: any;
+
+    constructor(options: Partial<utils.AdapterOptions> = {}) {
+        super({ ...options, name: "stromgedacht" });
+        this.on("ready", this.onReady.bind(this));
+        this.on("unload", this.onUnload.bind(this));
+    }
+
+    async onReady() {
+        await this.createStates();
+        await this.updateData();
+
+        const interval = (this.config.updateInterval || 5) * 60 * 1000;
+        this.interval = setInterval(() => this.updateData(), interval);
+    }
+
+    async createStates() {
+        await this.setObjectNotExistsAsync("current.level",{type:"state",common:{name:"Level",type:"number",role:"indicator",read:true,write:false},native:{}});
+        await this.setObjectNotExistsAsync("current.name",{type:"state",common:{name:"Name",type:"string",role:"text",read:true,write:false},native:{}});
+        await this.setObjectNotExistsAsync("current.text",{type:"state",common:{name:"Text",type:"string",role:"text",read:true,write:false},native:{}});
+    }
+
+    async updateData() {
+        try {
+            const res = await axios.get("https://api.stromgedacht.de/v1/data");
+            const data = res.data;
+
+            await this.setStateAsync("current.level", data.current.level, true);
+            await this.setStateAsync("current.name", data.current.name, true);
+            await this.setStateAsync("current.text", data.current.text, true);
+
+            if (data.forecast) {
+                for (let i=0;i<Math.min(data.forecast.length, this.config.forecastHours || 24);i++){
+                    const f = data.forecast[i];
+                    const base = `forecast.${i}`;
+
+                    await this.setObjectNotExistsAsync(base+".level",{type:"state",common:{name:"Level",type:"number",role:"value",read:true,write:false},native:{}});
+                    await this.setObjectNotExistsAsync(base+".from",{type:"state",common:{name:"From",type:"string",role:"date",read:true,write:false},native:{}});
+                    await this.setObjectNotExistsAsync(base+".to",{type:"state",common:{name:"To",type:"string",role:"date",read:true,write:false},native:{}});
+
+                    await this.setStateAsync(base+".level", f.level, true);
+                    await this.setStateAsync(base+".from", f.from, true);
+                    await this.setStateAsync(base+".to", f.to, true);
+                }
+            }
+
+        } catch (e) {
+            this.log.error("API error: " + e);
+        }
+    }
+
+    onUnload(callback: () => void) {
+        if (this.interval) clearInterval(this.interval);
+        callback();
+    }
+}
+
+if (module.parent) {
+    module.exports = (options: Partial<utils.AdapterOptions>) => new StromGedacht(options);
+} else {
+    new StromGedacht();
+}
